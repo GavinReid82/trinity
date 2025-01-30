0fe4eece69816b4c1567a50e8b3972b9718cb22b
Gavin Reid <gavinjohnreid@gmail.com>
2025-01-30 13:28:36 +0100

    Remove Waitress and configure Gunicorn

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 from app import app
-from waitress import serve
-
-serve(app, host="127.0.0.1", port=5000, max_request_body_size=100 * 1024 * 1024)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
